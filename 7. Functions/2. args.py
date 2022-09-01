869af364df0f2c369e005a76be8448fa88daa5e2
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-09-01 10:27:40 +0200

    Update function file

---
+++ 7. Functions/2. args.py
@@ -0,0 +1,31 @@
+# *args (arguments)
+
+# *args behaves like a tuple of parameters that are coming in.
+# *args allows us to take random numbers of arguments.
+# * means a collection of arguments
+
+# EXAMPLE 1
+def myfunc(*args):
+  return sum(args)
+
+print(myfunc(40,60,100,50))
+
+
+# EXAMPLE 2
+def myfunc(*args):
+
+  for item in args:
+    print(item)
+
+
+print(myfunc(40,60,70,100))
+
+
+# EXAMPLE 3
+def multiply(*numbers):   # * means a collection of arguments
+  total = 1
+  for number in numbers:
+    total *= number
+  return total
+
+print(multiply(2, 3, 4, 5))
---
+++ 7. Functions/5. input.py
@@ -0,0 +1,8 @@
+#input function
+number= int(input('Enter a number here: '))
+
+# input always accepts any value as a string
+name = input('What is your name?')
+
+print(f"The number that you have entered is {number}\n"
+      f"The name is {name}")
