c349560b60af2d9da4a80708391a2d6b844ee9f0
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-09-01 15:28:52 +0200

    Update OOP file

---
+++ OOP/7. ComparingTwoObjects.py
@@ -0,0 +1,22 @@
+# 7. COMPARING TWO OBJECTS - with magic methods
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):  # Equal
+        return self.x == other.x and self.y == other.y
+
+    def __gt__(self, other):  # Greater than
+        return self.x > other.x and self.y > other.y
+
+
+point = Point(1, 2)
+other = Point(1, 2)
+print(point == other)  # Returns Tru
+
+point = Point(10, 20)
+other = Point(1, 2)
+print(point > other)    # Returns True
+print(point < other)    # Returns False
